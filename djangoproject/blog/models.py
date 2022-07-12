d9a681490557815254cea34bac5f09a45879ae6d
jersbluesky <jersbluesky@gmail.com>
2022-07-11 23:12:54 -0600

    done w/steps frm aksonai on YT, nxt: customize

--- djangoproject/blog/models.py
+++ djangoproject/blog/models.py
@@ -18,3 +18,15 @@ class Post(models.Model):
         return self.title
     
 
+# Using DJANGO ORM and Querysets:
+# Django shell: python3 manage.py shell
+# - Also, from blog.models import Post (in other words, import the class from the 
+# - - - app.models)
+# -- to list all objects (not details)--> [Object].objects.all(): 
+# - - - example-> Post.objects.all()
+# -- to list specific objects (ex. author) - import from django.contrib.auth.models import User
+# - - - (this is something embedded in the framework)
+# - - - then: User.objects.all()
+# -- to GET a property from an object: [PropertyKeyWord].obreate objects: reate objects: jects.get([property] = '[propertyname]')
+# - - - example: User.objects.get(username='ubuntu1')
+# -- You can get and put it in a variable
